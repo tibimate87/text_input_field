6bd11647a1eca72fefd19f5b24928fbc6a853a4f
tibimate87 <tibormate87@gmail.com>
2021-01-28 11:03:50 +0100

    first commit

--- lib/text_input_field.dart
+++ lib/text_input_field.dart
@@ -1,7 +1,80 @@
 library text_input_field;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class TextFieldInput extends StatelessWidget {
+  final String labelText;
+  final int maxLines;
+  final Function onChanged;
+  final Function onTap;
+  final TextEditingController controller;
+  final bool isWarningVisible;
+  final IconData icon;
+  final Function iconPressed;
+  final Color textColor;
+  final String fontFamily;
+
+  bool isPassword;
+  TextFieldInput(
+      {@required this.labelText,
+      this.maxLines = 1,
+      this.isPassword = false,
+      this.onChanged,
+      this.onTap,
+      this.isWarningVisible = false,
+      this.textColor = Colors.white,
+      this.fontFamily,
+      @required this.controller,
+      this.icon,
+      this.iconPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      maxLength: 5,
+      obscureText: isPassword,
+      controller: controller,
+      maxLines: maxLines,
+      cursorColor: textColor,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: textColor,
+        fontSize: 15,
+        fontFamily: fontFamily,
+      ),
+      decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25.0),
+            borderSide: BorderSide(
+              color: textColor,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25.0),
+            borderSide: BorderSide(
+              color: textColor,
+            ),
+          ),
+          labelText: labelText,
+          labelStyle: TextStyle(
+            color: textColor,
+            fontFamily: fontFamily,
+          ),
+          border: new OutlineInputBorder(
+            borderRadius: new BorderRadius.circular(25.0),
+            borderSide: new BorderSide(),
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(icon),
+            onPressed: () => iconPressed(),
+          )),
+      onChanged: (value) {
+        onChanged(value);
+      },
+      onTap: () {
+        onTap();
+      },
+    );
+  }
 }
